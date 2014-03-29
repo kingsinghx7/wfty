c2fe9a2398d891b91da83e6d0bb3da4f94d9da4a
Ryan Singh <kingsinghx7@gmail.com>
2014-03-28 22:24:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whootafutty::Application.config.secret_key_base = '85bb0b12fa5252230738b170fc6de3815d4e31c33bac5816da9b23158e82949d0df5bfb10306312df95f7e201c36f171136fa4faf3c550acd1268aefb3bc3ae1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whootafutty::Application.config.session_store :cookie_store, key: '_whootafutty_session'
